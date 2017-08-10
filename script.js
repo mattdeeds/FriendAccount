a94d3ff503c79e8934147113d95b2bc861d35595
mattdeeds <mdeeds@0587351617.wireless.umich.net>
2017-08-10 10:15:32 -0400

    Fixed javascript

--- script.js
+++ script.js
@@ -8,18 +8,23 @@ function checkEmail() {
 	var test = "test@umich.edu"
 	//Call function here that compares email var with email already in database. Make it return a bool which is checked in if statement below
 	if (email == test) {
-		$('#warning').toggle();
+		$('#warning').show();
 	} else {
     	//alert("Email has been sent to " + email);
     	$('#warning').hide();
+    	window.location.href = "registration_personalInfo.html";
+
 	}
-	
 }
 function checkPass() {
+	var password = document.getElementById("pwd").value;
+	var passwordConf = document.getElementById("confirmPwd").value;
+
 	if (password != passwordConf) {
 		$('#passWarning').show();
 	} else {
 		$('#passWarning').hide();
+		window.location.href = "registration_securityQuestions.html";
 	}
 }
 function hashPass() {
